965b52f8b65c2be7a4c34ccadf375f8f3fd6ddcb
Lucad44 <dintronoluca758@gmail.com>
2023-12-03 21:58:00 +0100

    day3_2

--- 2023/day3_2.py
+++ 2023/day3_2.py
@@ -1,32 +1,60 @@
 import re
 
 
-def is_symbol(c):
-    if ('!' <= c <= '/' and c != '.') or (':' <= c <= '@') or ('[' <= c <= '`') or ('{' <= c <= '~'):
-        return True
-    return False
-
-with open("input.txt", "r") as f:
+with open("2023/input.txt", "r") as f:
     lines = [line.rstrip() for line in f]
 
-ans = 0
-for i in range(len(lines)):
-    matches = [(int(match.group()), match.start(), match.end() - 1) for match in re.finditer(r'\d+', lines[i])]
+dict_list = []
+for i, line in enumerate(lines):
+    matches = [(int(match.group()), match.start(), match.end() - 1) for match in re.finditer(r'\d+', line)]
     num_dict = {}
     for num, start, end in matches:
         if num not in num_dict:
             num_dict[num] = []
         num_dict[num].extend([start, end])
+    dict_list.append(num_dict)
 
-    for k, v in num_dict.items():
-        for t in range(0, len(v), 2):
-            if (v[0] and is_symbol(lines[i][v[0] - 1])) or (v[1] < len(lines[i]) - 1 and is_symbol(lines[i][v[1] + 1])):
-                print(k, v[t], v[t + 1])
-                ans += k
-                continue
-            for l in range(max(0, v[t] - 1), min(len(lines[i]), v[t + 1] + 2)):
-                if ((i and is_symbol(lines[i - 1][l])) or (i < len(lines) - 1 and is_symbol(lines[i + 1][l]))):
-                    print(k, v[t], v[t + 1])
-                    ans += k
+ans = 0
+for i, line in enumerate(lines):
+    for j in range(len(line)):
+        coords = []
+        if line[j] == '*':
+            if j and line[j - 1].isdigit():
+                coords.append(j - 1)
+            if j < len(line) - 1 and line[j + 1].isdigit():
+                coords.append(j + 1)
+
+            general_flag = True
+            f1, f2 = True, True
+            for l in range(max(0, j - 1), min(len(line), j + 2)):
+                if f1 and i and lines[i - 1][l].isdigit():
+                    coords.append(l)
+                    f1 = False
+                elif not f1 and i and lines[i - 1][l].isdigit() and not lines[i - 1][l - 1].isdigit():
+                    general_flag = False
+                    break
+                if f2 and i < len(lines) -1 and lines[i + 1][l].isdigit():
+                    coords.append(l)
+                    f2 = False
+                elif not f2 and i < len(lines) - 1 and lines[i + 1][l].isdigit() and not lines[i + 1][l - 1].isdigit():
+                    general_flag = False
                     break
 
+        if len(coords) != 2 or not general_flag:
+            continue
+        
+        print(i, coords)
+
+        prod = 1
+        for l, d in enumerate(dict_list):
+            if l < i - 1:
+                continue
+            if l > i + 1:
+                break
+            for k, v in d.items():
+                for t in range(0, len(v), 2):
+                    if (v[t] <= coords[0] <= v[t + 1]) or (v[t] <= coords[1] <= v[t + 1]):
+                        prod *= k
+        ans += prod
+
+print(ans)
